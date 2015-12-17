b1aa4e6eb22b9397a055ca3a9c8b68291a97fa13
Ryan Priebe <rpriebe@me.com>
2015-12-17 13:06:28 -0700

    fixed create_tag test by using a variable tag name

--- test/test_tag.rb
+++ test/test_tag.rb
@@ -16,8 +16,9 @@ class TagTest < Minitest::Test
 
   def test_tag_create
     tag = Evertils::Common::Entity::Tag.new
-    
-    assert tag.create('et_test_tag')
-    assert tag.expunge('et_test_tag')
+
+    date = DateTime.now.to_s
+    assert tag.create(date).is_a? Evernote::EDAM::Type::Tag
+    assert tag.expunge(date)
   end
 end
