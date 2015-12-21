24a58ed6b22ed39b6df64757ef3690583f1ec027
Ryan Priebe <rpriebe@me.com>
2015-12-21 15:39:23 -0700

    handle rate limit message right in authentication model

--- lib/evertils/common/authentication.rb
+++ lib/evertils/common/authentication.rb
@@ -4,32 +4,42 @@ module Evertils
       attr_accessor :store, :shardId, :version
 
       def initialize
-        if Evertils::Common::EVERNOTE_DEVELOPER_TOKEN.nil?
-          Notify.error("Evernote developer token is not configured properly!\n$EVERTILS_TOKEN == nil")
+        begin
+          if Evertils::Common::EVERNOTE_DEVELOPER_TOKEN.nil?
+            Notify.error("Evernote developer token is not configured properly!\n$EVERTILS_TOKEN == nil")
+          end
+
+          userStoreUrl = "https://#{Evertils::Common::EVERNOTE_HOST}/edam/user"
+
+          userStoreTransport = Thrift::HTTPClientTransport.new(userStoreUrl)
+          userStoreProtocol = Thrift::BinaryProtocol.new(userStoreTransport)
+          @@user = ::Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)
+
+          versionOK = @@user.checkVersion("evernote-data",
+                     ::Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
+                     ::Evernote::EDAM::UserStore::EDAM_VERSION_MINOR)
+
+          @version = "#{::Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR}.#{::Evernote::EDAM::UserStore::EDAM_VERSION_MINOR}"
+          @shardId = user.shardId
+
+          if !versionOK
+            Notify.error("Evernote API requires an update.  Latest version is #{@version}")
+          end
+
+          noteStoreUrl = @@user.getNoteStoreUrl(Evertils::Common::EVERNOTE_DEVELOPER_TOKEN)
+
+          noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
+          noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
+          @store = ::Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
+        rescue Evernote::EDAM::Error::EDAMSystemException => e
+          Notify.warning("Problem authenticating, EDAM code #{e.errorCode}")
+
+          case e.errorCode
+          when 19
+            minutes = (e.rateLimitDuration/60).to_i
+            Notify.warning("You are rate limited!  Wait #{minutes} minutes")
+          end
         end
-
-        userStoreUrl = "https://#{Evertils::Common::EVERNOTE_HOST}/edam/user"
-
-        userStoreTransport = Thrift::HTTPClientTransport.new(userStoreUrl)
-        userStoreProtocol = Thrift::BinaryProtocol.new(userStoreTransport)
-        @@user = ::Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)
-
-        versionOK = @@user.checkVersion("evernote-data",
-                   ::Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
-                   ::Evernote::EDAM::UserStore::EDAM_VERSION_MINOR)
-
-        @version = "#{::Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR}.#{::Evernote::EDAM::UserStore::EDAM_VERSION_MINOR}"
-        @shardId = user.shardId
-
-        if !versionOK
-          Notify.error("Evernote API requires an update.  Latest version is #{@version}")
-        end
-
-        noteStoreUrl = @@user.getNoteStoreUrl(Evertils::Common::EVERNOTE_DEVELOPER_TOKEN)
-
-        noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
-        noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
-        @store = ::Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
       end
 
       def info
