37d5e32656137e3a67c564b1e3d821d695c74138
Ryan Priebe <rpriebe@me.com>
2015-12-21 15:41:04 -0700

    delete the backup note as soon as we're done testing it's existence

--- test/test_backup_queries.rb
+++ test/test_backup_queries.rb
@@ -10,7 +10,10 @@ class BackupQueriesTest < Minitest::Test
   # @since 0.2.8
   def test_files
     backup = @entity.files(__FILE__)
+    note_manager = Evertils::Common::Entity::Note.new
 
     assert backup.is_a? Hash
+    
+    note_manager.expunge(backup[:note].title)
   end
 end
