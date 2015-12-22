84dabd0b0cc570fa5c15cdd50c2b59946ae57a4f
Ryan Priebe <rpriebe@me.com>
2015-12-22 11:36:27 -0700

    weird duplicate method removed

--- lib/evertils/common/converter.rb
+++ lib/evertils/common/converter.rb
@@ -9,12 +9,6 @@ module Evertils
           puts "Deprecated as of #{version}"
         end
 
-        #
-        # @since 0.2.8
-        def deprecation_notice(version)
-          puts "Deprecated as of #{version}"
-        end
-
       end
     end
   end
