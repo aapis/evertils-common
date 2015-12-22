66ac7923b780206d24da09bc0bbb995283451f50
Ryan Priebe <rpriebe@me.com>
2015-12-22 11:36:40 -0700

    added stubs for converting from string and file

--- lib/evertils/common/converter/yaml_to_enml.rb
+++ lib/evertils/common/converter/yaml_to_enml.rb
@@ -3,6 +3,18 @@ module Evertils
     module Converter
       class YamlToEnml < Converter::Base
         
+        #
+        # @since 0.2.9
+        def from_file(path)
+
+        end
+
+        #
+        # @since 0.2.9
+        def from_string(yaml)
+
+        end
+
       end
     end
   end
