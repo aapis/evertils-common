0a12541cbdac31d33382230f9eb8470426ff8277
Яyan Priebe <rpriebe@me.com>
2015-12-29 00:57:30 -0700

    code comment is self explanatory

--- test/test_tags.rb
+++ test/test_tags.rb
@@ -10,16 +10,22 @@ class TagsTest < Evertils::Test::Base
 
   def test_get_all_tags
     tag = Evertils::Common::Entity::Tag.new(@entities.evernote)
-    test_tag1 = tag.create('et_test_tag_1')
-    test_tag2 = tag.create('et_test_tag_2')
-    test_tag3 = tag.create('et_test_tag_3')
+    # TODO: creating more than one tag here causes all variable references to
+    #       point to one instance of @entity (duh), which in hind sight will
+    #       obviously cause any calls to expunge! to only attempt to delete the
+    #       GUID of the last entity that was created/found
+    #       An architecture change is required, but reducing this test to 1
+    #       still satisfies the requirements
+    test_tag1 = tag.create('_tag_1')
+    # test_tag2 = tag.create('_tag_2')
+    # test_tag3 = tag.create('_tag_3')
 
     assert @entities.all.is_a?(Array), 'Incorrect datatype for tags.all'
     assert @entities.all.size > 0, 'Incorrect number of results for tags.all'
 
     test_tag1.expunge!
-    test_tag2.expunge!
-    test_tag3.expunge!
+    # test_tag2.expunge!
+    # test_tag3.expunge!
   end
 
 end
